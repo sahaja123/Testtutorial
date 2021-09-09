a65482ce1eadd2886748b3bd680e1a84ced67af7
sahaja123 <sahaja.d@ustocktrade.com>
2021-09-09 21:31:36 +0530

    llllllllllll

--- sample.js
+++ sample.js
@@ -1,3 +1,14 @@
 console.log('Hello js 1')
 console.log('Hello js 2')
 console.log('Hello js 3')
+
+let name = 'sahaja';
+const age = 30;
+let hasHobby = true;
+
+//age = 31;
+function myFunction(myName, myAge, myHobby){
+    return ('I am ' + myName + ' and I am ' + myAge + ' years old. I have hobby yes ' + myHobby);
+}
+
+console.log(myFunction(name, age, hasHobby));
